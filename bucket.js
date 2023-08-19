62fbde7ed62a1d19579fa5543e509126da670eb8
shahina42 <141030593+shahina42@users.noreply.github.com>
2023-08-18 22:41:04 -0700

    Add files via upload

---
+++ bucket.js
@@ -0,0 +1,38 @@
+var addbtn = document.querySelector("#btn");
+var inp = document.querySelector(".bo");
+var box = document.querySelector(".listbox");
+var res = document.querySelector(".box");
+
+var arr = [];
+
+
+function remove(i) {
+    arr.splice(i, 1);
+    console.log(arr)
+    display()
+}
+
+function display() {
+    box.innerHTML = ""
+    for (let i = 0; i < arr.length; i++) {
+        console.log(i)
+        box.innerHTML += `<div class="item">${arr[i]}<span class="tick"><img src="https://kq-storage.s3.ap-south-1.amazonaws.com/Github/bucketlist/tick-mark%5B1%5D.png" alt="" onclick="remove(${i})"></span></div><br/>`
+    }
+}
+
+function addtoarray() {
+    arr.push(inp.value)
+    display()
+}
+
+
+addbtn.onclick = function shahina() {
+    console.log(inp.value);
+    addtoarray()
+
+}
+
+res.onclick = function reset() {
+    arr = []
+    display()
+}
